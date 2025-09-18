bbbb79e58befb0b24adc388fbacc0018926c212f
amesul <samuelstroesser@icloud.com>
2025-09-18 16:25:30 +0200

    Refactor token models: replace `$fillable` with `$guarded`, simplify attribute methods, and adjust casts

--- app/Models/HelloassoToken.php
+++ app/Models/HelloassoToken.php
@@ -8,34 +8,22 @@ use Illuminate\Support\Facades\Crypt;
 
 class HelloassoToken extends Model
 {
-    protected $fillable = [
-        'access_token',
-        'refresh_token',
-        'invalidated',
-        'expires_at',
-    ];
+    protected $guarded = [];
 
     protected function casts(): array
     {
         return [
-            'invalidated' => 'boolean',
             'expires_at' => 'datetime',
         ];
     }
 
     protected function accessToken()
     {
-        return Attribute::make(
-            get: fn($value) => Crypt::decryptString($value),
-            set: fn($value) => Crypt::encryptString($value),
-        );
+        return Attribute::make(get: fn($value) => Crypt::decryptString($value), set: fn($value) => Crypt::encryptString($value));
     }
 
     protected function refreshToken()
     {
-        return Attribute::make(
-            get: fn($value) => Crypt::decryptString($value),
-            set: fn($value) => Crypt::encryptString($value),
-        );
+        return Attribute::make(get: fn($value) => Crypt::decryptString($value), set: fn($value) => Crypt::encryptString($value));
     }
 }
--- app/Models/TwitchToken.php
+++ app/Models/TwitchToken.php
@@ -6,16 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class TwitchToken extends Model
 {
-    protected $fillable = [
-        'access_token',
-        'invalidated',
-        'expires_at',
-    ];
-
-    protected function casts(): array
-    {
-        return [
-            'invalidated' => 'boolean',
-        ];
-    }
+    protected $guarded = [];
 }
