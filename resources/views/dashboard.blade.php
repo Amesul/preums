afdc468deb867fe21fa9a7ed75f6ca5b2cbf5614
amesul <samuelstroesser@icloud.com>
2025-09-18 14:25:30 +0200

    Fix indentation in dashboard layout for consistent formatting

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -2,21 +2,21 @@
     <div class="flex h-full w-full flex-1 grow flex-col gap-4 rounded-xl">
         <div class="grid auto-rows-min gap-4 md:grid-cols-3  shrink-0 ">
             <div
-                class="relative overflow-hidden rounded-xl h-36 flex justify-around align-middle border border-neutral-200 dark:border-neutral-700">
+                    class="relative overflow-hidden rounded-xl h-36 flex justify-around align-middle border border-neutral-200 dark:border-neutral-700">
                 <livewire:total-donations/>
             </div>
             <div
-                class="relative overflow-hidden flex rounded-xl h-36 border border-neutral-200 dark:border-neutral-700">
+                    class="relative overflow-hidden flex rounded-xl h-36 border border-neutral-200 dark:border-neutral-700">
                 <livewire:twitch-viewers-count/>
             </div>
             <div
-                class="relative overflow-hidden flex rounded-xl h-36 border border-neutral-200 dark:border-neutral-700">
+                    class="relative overflow-hidden flex rounded-xl h-36 border border-neutral-200 dark:border-neutral-700">
                 <livewire:unprocessed-donations/>
             </div>
         </div>
         <div class="relative  flex-1 w-full shrink-0 ">
             <div
-                class="absolute h-full w-full overflow-y-scroll hidden md:block rounded-xl border border-neutral-200 dark:border-neutral-700">
+                    class="absolute h-full w-full overflow-y-scroll hidden md:block rounded-xl border border-neutral-200 dark:border-neutral-700">
                 <livewire:index-donations/>
             </div>
         </div>
