8a42d8b95ba817e8bc4407710b2b9280f620a8e4
amesul <samuelstroesser@icloud.com>
2025-09-18 14:24:58 +0200

    Refactor HelloAsso service: fix namespace, replace hardcoded URLs with config, and improve token handling logic

---
+++ app/Services/HelloAsso.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\HelloassoToken;
+use Illuminate\Http\Client\ConnectionException;
+use Illuminate\Support\Facades\Http;
+
+class HelloAsso
+{
+    /**
+     * @throws ConnectionException
+     */
+    public static function refreshToken(): HelloassoToken
+    {
+        $token = HelloassoToken::where('invalidated', '=', false)
+                               ->first();
+        $res = Http::asForm()
+                   ->post(config('helloasso.oauth_url'), [
+                       'client_id'     => config('helloasso.client_id'),
+                       'grant_type'    => 'refresh_token',
+                       'refresh_token' => $token?->refresh_token,
+                   ]);
+        if ($res->status() !== 200) {
+            $token?->update([
+                'invalidated' => true,
+            ]);
+            return self::getToken();
+        } else {
+            $res = $res->json();
+            $token->update([
+                'access_token' => $res['access_token'],
+                'expires_at'   => now()->addSeconds($res['expires_in']),
+            ]);
+            return $token;
+        }
+    }
+
+    /**
+     * @throws ConnectionException
+     */
+    public static function getToken(): HelloassoToken
+    {
+        $res = Http::asForm()
+                   ->post(config('helloasso.oauth_url'), [
+                       'grant_type'    => 'client_credentials',
+                       'client_id'     => config('helloasso.client_id'),
+                       'client_secret' => config('helloasso.client_secret'),
+                   ]);
+        if ($res->status() === 200) {
+            $jsonRes = $res->json();
+            $token = [
+                'access_token'  => $jsonRes['access_token'],
+                'expires_at'    => now()->addSeconds($jsonRes['expires_in']),
+                'refresh_token' => $jsonRes['refresh_token'],
+                'invalidated'   => false,
+            ];
+            return HelloassoToken::create($token);
+        } else {
+            throw new ConnectionException($res->getReasonPhrase(), $res->status());
+        }
+    }
+}
