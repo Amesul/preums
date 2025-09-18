a897afc52ed48fa8fd429ec5dbf7896faac7c581
amesul <samuelstroesser@icloud.com>
2025-09-18 16:24:39 +0200

    Refactor HelloAsso service: improve token refresh logic, handle 400 errors, and enhance exception handling

--- app/Services/HelloAsso.php
+++ app/Services/HelloAsso.php
@@ -4,6 +4,8 @@ namespace App\Services;
 
 use App\Models\HelloassoToken;
 use Illuminate\Http\Client\ConnectionException;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Routing\Redirector;
 use Illuminate\Support\Facades\Http;
 
 class HelloAsso
@@ -11,28 +13,30 @@ class HelloAsso
     /**
      * @throws ConnectionException
      */
-    public static function refreshToken(): HelloassoToken
+    public static function refreshToken(): HelloassoToken|RedirectResponse|Redirector
     {
-        $token = HelloassoToken::where('invalidated', '=', false)
+        $token = HelloassoToken::all()
                                ->first();
+
         $res = Http::asForm()
                    ->post(config('helloasso.oauth_url'), [
                        'client_id'     => config('helloasso.client_id'),
                        'grant_type'    => 'refresh_token',
-                       'refresh_token' => $token?->refresh_token,
+                       'refresh_token' => $token->refresh_token,
                    ]);
-        if ($res->status() !== 200) {
-            $token?->update([
-                'invalidated' => true,
-            ]);
-            return self::getToken();
-        } else {
+        if ($res->status() === 200) {
             $res = $res->json();
             $token->update([
-                'access_token' => $res['access_token'],
-                'expires_at'   => now()->addSeconds($res['expires_in']),
+                'access_token'  => $res['access_token'],
+                'expires_at'    => now()->addSeconds($res['expires_in']),
+                'refresh_token' => $res['refresh_token']
             ]);
             return $token;
+        } elseif ($res->status() === 400) {
+            $token?->delete();
+            return self::getToken();
+        } else {
+            throw new ConnectionException($res->getReasonPhrase(), $res->status());
         }
     }
 
