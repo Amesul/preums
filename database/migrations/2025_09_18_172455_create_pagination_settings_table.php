c20ada0ae3b87151715cdde1db912d601936dc12
amesul <samuelstroesser@icloud.com>
2025-09-18 21:01:01 +0200

    Add migration for `pagination_settings` table creation

---
+++ database/migrations/2025_09_18_172455_create_pagination_settings_table.php
@@ -0,0 +1,24 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    public function up(): void
+    {
+        Schema::create('pagination_settings', function (Blueprint $table) {
+            $table->id();
+            $table->integer('value');
+            $table->foreignIdFor(User::class)
+                  ->constrained('users');
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('pagination_settings');
+    }
+};
