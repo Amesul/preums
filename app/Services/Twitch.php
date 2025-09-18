8ffb6242f1f079f07c92d6d4c9dd50bd9803ce25
amesul <samuelstroesser@icloud.com>
2025-09-18 14:23:45 +0200

    Update Twitch service: refine viewer count handling, use configurable user, improve error management

---
+++ app/Services/Twitch.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\TwitchToken;
+use Illuminate\Support\Facades\Http;
+
+class Twitch
+{
+    public static function GetViewerCount(): int|string
+    {
+        $res = Http::withHeaders([
+            'Authorization' => 'Bearer ' . TwitchToken::where('invalidated', '=', false)
+                                                      ->first()?->access_token,
+            'Client-Id'     => config('twitch.client_id'),
+        ])
+                   ->withQueryParameters([
+                       'user_login' => 'chachamaxx',
+                   ])
+                   ->get(config('twitch.base_url') . '/streams');
+        if ($res->status() === 200) {
+            $res = $res->json();
+            if ($res['data']) {
+                return $res['data'][0]['viewer_count'];
+            } else {
+                return 'Hors ligne';
+            }
+        } elseif ($res->status() === 401) {
+            TwitchToken::where('invalidated', '=', false)
+                       ->first()
+                       ?->update(['invalidated' => true]);
+            self::GetAppAccessToken();
+            return self::GetViewerCount();
+        } else {
+            return 'Erreur';
+        }
+    }
+
+    public static function GetAppAccessToken(): TwitchToken
+    {
+        $res = Http::asForm()
+                   ->post('https://id.twitch.tv/oauth2/token', [
+                       'client_id'     => config('twitch.client_id'),
+                       'client_secret' => config('twitch.client_secret'),
+                       'grant_type'    => 'client_credentials',
+                   ]);
+        $res = $res->json();
+        return TwitchToken::create([
+            'access_token' => $res['access_token'],
+            'expires_at'   => now()->addSeconds($res['expires_in']),
+        ]);
+    }
+}
