8b8cb0e6e1fc9f9b3ff5cee4a99eb3fe6061c77a
amesul <samuelstroesser@icloud.com>
2025-09-18 14:24:33 +0200

    Add OAuth URL configuration to HelloAsso settings

--- config/helloasso.php
+++ config/helloasso.php
@@ -3,6 +3,7 @@
 
 return [
     'api_base_url' => env('HELLOASSO_BASE_URL'),
+    'oauth_url' => env('HELLOASSO_OAUTH_URL'),
     'client_secret' => env('HELLOASSO_CLIENT_SECRET'),
     'client_id' => env('HELLOASSO_CLIENT_ID'),
 ];
