c7ad375de24f607b472bd4bb6f721017aa24312b
amesul <samuelstroesser@icloud.com>
2025-09-18 18:37:10 +0200

    Update dashboard styles: refine dark mode colors and fix inconsistencies in component backgrounds

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -2,24 +2,24 @@
     <div class="flex h-full w-full flex-1 grow flex-col gap-4 rounded-xl">
         <div class="grid shrink-0 auto-rows-min gap-4 md:grid-cols-3">
             <div
-                class="relative flex h-36 justify-around overflow-hidden rounded-xl border border-neutral-400 bg-neutral-50 align-middle dark:border-neutral-700"
+                class="relative flex h-36 justify-around overflow-hidden rounded-xl border border-neutral-400 bg-neutral-50 align-middle dark:border-neutral-600 dark:bg-slate-800"
             >
                 <livewire:total-donations />
             </div>
             <div
-                class="relative flex h-36 overflow-hidden rounded-xl border border-neutral-400 bg-neutral-50 dark:border-neutral-700"
+                class="relative flex h-36 overflow-hidden rounded-xl border border-neutral-400 bg-neutral-50 dark:border-neutral-600 dark:bg-slate-800"
             >
                 <livewire:twitch-viewers-count />
             </div>
             <div
-                class="relative flex h-36 overflow-hidden rounded-xl border border-neutral-400 bg-neutral-50 dark:border-neutral-700"
+                class="relative flex h-36 overflow-hidden rounded-xl border border-neutral-400 bg-neutral-50 dark:border-neutral-600 dark:bg-slate-800"
             >
                 <livewire:unprocessed-donations />
             </div>
         </div>
         <div class="relative w-full flex-1 shrink-0">
             <div
-                class="absolute hidden h-full w-full overflow-y-scroll rounded-xl border border-neutral-400 bg-neutral-50 md:block dark:border-neutral-700"
+                class="absolute hidden h-full w-full overflow-y-scroll rounded-xl border border-neutral-400 md:block dark:border-neutral-600"
             >
                 <livewire:index-donations />
             </div>
