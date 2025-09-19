a9e47fd529ddc9280a1b4174753fcb59f849878e
amesul <samuelstroesser@icloud.com>
2025-09-19 23:33:59 +0200

    Update `total-donations.blade.php`: adjust layout with full-height and width styles, add transparent background for consistency

--- resources/views/total-donations.blade.php
+++ resources/views/total-donations.blade.php
@@ -1,9 +1,12 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace("_", "-", app()->getLocale()) }}" class="dark">
-    <head>
-        @include("partials.head")
-    </head>
-    <body class="flex min-h-screen justify-center align-middle">
-        <livewire:donations-widget />
-    </body>
+<html
+        lang="{{ str_replace("_", "-", app()->getLocale()) }}"
+        class="dark h-full w-full bg-transparent"
+>
+<head>
+    @include("partials.head")
+</head>
+<body class="flex min-h-screen justify-center bg-transparent align-middle">
+<livewire:donations-widget />
+</body>
 </html>
