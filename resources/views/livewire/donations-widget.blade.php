b62ccdde5429769863e7c512ce4e4cf7e515bb7e
amesul <samuelstroesser@icloud.com>
2025-09-19 23:38:02 +0200

    Update `donations-widget.blade.php`: increase text size for total donations display to improve readability

--- resources/views/livewire/donations-widget.blade.php
+++ resources/views/livewire/donations-widget.blade.php
@@ -1,3 +1,3 @@
-<div wire:poll.keep-alive="refresh" class="font-cabin-sketch text-7xl font-bold w-full text-right px-2 py-4">
+<div wire:poll.keep-alive="refresh" class="font-cabin-sketch text-8xl font-bold w-full text-right px-2 py-4">
     {{ $total }} €
 </div>
