f8359906abaead444930563f5538bad0150bc48b
amesul <samuelstroesser@icloud.com>
2025-09-19 13:34:33 +0200

    Update `donations-widget.blade.php`: adjust layout with padding and full-width styling

--- resources/views/livewire/donations-widget.blade.php
+++ resources/views/livewire/donations-widget.blade.php
@@ -1,3 +1,3 @@
-<div wire:poll.keep-alive="refresh" class="font-cabin-sketch text-8xl text-right">
+<div wire:poll.keep-alive="refresh" class="font-cabin-sketch text-8xl w-full text-right px-2 py-4">
     {{ $total }} €
 </div>
