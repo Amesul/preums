576408c8c4f5c9acfd16150f6d87fdb150406343
amesul <samuelstroesser@icloud.com>
2025-09-19 03:33:44 +0200

    Update `app-logo.blade.php`: fix minor formatting issue in `img` tag

--- resources/views/components/app-logo.blade.php
+++ resources/views/components/app-logo.blade.php
@@ -1,7 +1,7 @@
 <div
         class="flex aspect-square size-8 items-center justify-center rounded-md bg-accent-content text-accent-foreground"
 >
-    <img src="{{ url('./storage/images/insigne_preums.png') }}" alt="Logo" class="size-8 aspect-square rounded-sm"/>
+    <img src="{{ url('./storage/images/insigne_preums.png') }}" alt="Logo" class="size-8 aspect-square rounded-sm" />
 </div>
 <div class="ms-1 grid flex-1 text-start text-sm">
     <span class="mb-0.5 truncate leading-tight font-semibold">
