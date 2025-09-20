8a4f4038dfdd05b50f366ab21a9e012d0d852749
amesul <samuelstroesser@icloud.com>
2025-09-20 14:51:15 +0200

    Update `donations-widget.blade.php`: restore dynamic total, comment out static value for clarity

--- resources/views/livewire/donations-widget.blade.php
+++ resources/views/livewire/donations-widget.blade.php
@@ -1,7 +1,7 @@
-{{--<div wire:poll.keep-alive="refresh" class="font-cabin-sketch text-8xl font-bold w-full text-right px-2 py-4">--}}
-{{--    {{ $total }} €--}}
-{{--</div>--}}
-
 <div wire:poll.keep-alive="refresh" class="font-cabin-sketch text-8xl font-bold w-full text-right px-2 py-4">
-    {{ 1312.00 }} €
+    {{ $total }} €
 </div>
+
+{{--<div wire:poll.keep-alive="refresh" class="font-cabin-sketch text-8xl font-bold w-full text-right px-2 py-4">--}}
+{{--    1312.00  €--}}
+{{--</div>--}}
